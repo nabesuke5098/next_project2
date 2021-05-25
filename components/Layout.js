c65c343760b31814ad7f43e4d31c4a220c099bec
nabesuke <nabesuke5098@gmail.com>
2021-05-25 12:28:14 +0900

    [modified]footer

--- components/Layout.js
+++ components/Layout.js
@@ -10,15 +10,7 @@ export default function Layout({ children, title = "Default title" }) {
         {children}
       </main>
       <footer className="w-full h-12 flex justify-center items-center border-t">
-        <a
-          className="flex items-center"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by
-          <img src="/vercel.svg" alt="Vercel Logo" className="h-4 ml-2" />
-        </a>
+        @test nabesuke
       </footer>
     </div>
   );
